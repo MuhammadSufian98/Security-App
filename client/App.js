d70ce32133af28804a9bafe20da4582e46c36eee
MuhammadSufian98 <sufianhassan98@gmail.com>
2025-05-08 20:59:38 +0500

    first commit

---
+++ client/App.js
@@ -0,0 +1,24 @@
+import React from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import Header from "./components/header";
+import MainBody from "./components/mainBody";
+import liveFeed from "./components/liveFeed";
+
+const Stack = createNativeStackNavigator();
+
+export default function App() {
+  return (
+    <NavigationContainer>
+      <Header />
+      <Stack.Navigator
+        initialRouteName="Main"
+        screenOptions={{ headerShown: false }}
+      >
+        
+        <Stack.Screen name="Main" component={MainBody} />
+        <Stack.Screen name="liveFeed" component={liveFeed} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
